dcbec905d33ca1bfc7b0f1de9d54fe82d01059bc
Boris Jekov <jekovboris@gmail.com>
2024-12-30 14:01:43 +0100

    Cards fonctionnel

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './style.scss'; // Assurez-vous que le chemin du fichier SCSS est correct
 
+export default function Card({ image, title }) {
+  console.log("Card props:", { image, title }); // Vérification des valeurs des props
 
-function Card({image, title}) {
-    return (
-      <div className="card">
-        <img src={image} alt={title} className="card_img" />
-        <div className="card_overlay"></div>
-
-        <h2 className="card_title">{title}</h2>
+  return (
+    <article className="card-logement">
+      {/* Vérification de l'image */}
+      <img src={image} alt={title} className="card-img" />
+      
+      <div className="card-logement__layer">
+        <h2 className="card-logement__title">{title}</h2>
       </div>
-    );
+    </article>
+  );
 }
-
-export default Card;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import Gallery from "../../components/Gallery";
-import Banner from "../../components/Banner"; 
+import { Link } from 'react-router-dom';
+import Banner from "../../components/Banner";
+import Card from "../../components/Cards"; // Assurez-vous que le chemin est correct
 import Footer from "../../components/Footer";
-import homeBanner from "../../assets/banner/homeBanner.png"; // importer l'image ici
+import homeBanner from "../../assets/banner/homeBanner.png"; // L'image de la bannière
+import logementsData from "../../data/logements.json"; // Importer les données JSON directement
 
 const HomePage = () => {
+  // Vérification des données
+  console.log(logementsData); 
+
   return (
     <div className="home">
       <main>
-        <Banner texte="Chez vous, partout et ailleurs" image={homeBanner} /> {/* passer l'image importée ici */}
-        <Gallery />
+        {/* La bannière avec l'image importée et le texte */}
+        <Banner texte="Chez vous, partout et ailleurs" image={homeBanner} />
+        
+        {/* Affichage des cartes de logements */}
+        <div className="cards-container">
+          {logementsData.map((appart, id) => (
+            <div className="card_logement" key={id}>
+              <Link className="link_card_logement" to={`/logement/${appart.id}`}>
+                {/* Modifier 'cover' en 'image' */}
+                <Card image={appart.cover} title={appart.title} />
+              </Link>
+            </div>
+          ))}
+        </div>
       </main>
+
       <Footer />
     </div>
   );
